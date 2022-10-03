b99a4592596e0ee5450738a3efab234c7e05d4e4
= <rohit@fullstacklearning.com>
2022-10-03 12:38:40 +0530

    changed padding in header, added tech in training, added shadow in icons

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar({isActive , setIsActive ,handleClick}) {
           <div className="menu">
             
             <div className="dropdown">
-              <ul>
+              <ul className="mb-0">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#skills">Skills</a></li>
