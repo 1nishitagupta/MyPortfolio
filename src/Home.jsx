7103ebdeb0754d1d1df33254792c15fefcee65d9
Nishita Gupta <g.nishi9525@gmail.com>
2022-10-10 11:33:12 +0530

    commited

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import Navbar from "./Navbar/Navbar";
-import Banner from "./Banner/Banner";
-import About from "./About/About";
-import Skills from "./Skills/Skills";
-import Portfolio from "./Portfolio/Portfolio";
-import Qualification from "./Qualification/Qualification";
-import Services from "./Services/Services";
-import Contact from "./Contact/Contact";
-import Footer from "./Footer/Footer";
-import Hamburger from "./Navbar/Hamburger";
-import Blog from "./Blog";
+import Navbar from "./Components/Navbar";
+import Banner from "./Components/Banner";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Portfolio from "./Components/Portfolio";
+import Qualification from "./Components/Qualification";
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Hamburger from "./Components/Navbar/Hamburger";
+import Blog from "./Routes/Blog";
 import { Routes, Route } from "react-router-dom";
 
 function Home() {
@@ -41,7 +41,6 @@ function Home() {
         setIsActive={setIsActive}
         handleClick={handleClick}
       />
-
       <Routes>
         <Route path="blog" element={<Blog />} />
         <Route path="/" element={<HomePage />} />
--- src/Routes/Blog.jsx
+++ src/Routes/Blog.jsx
@@ -3,9 +3,6 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 
-
-
-
 function Blog() {
 
   const [posts, setPosts] = useState([])
